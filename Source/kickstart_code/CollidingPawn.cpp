02c7cc888592ac3e3420114c7b4796df18ea21d3
Lorenzo Salvadori <lorenzo.salvadori.86@gmail.com>
2022-01-05 23:35:55 +0100

    [Code] Started setting up a collidable sphere - root component and static mesh assigned

--- Source/kickstart_code/CollidingPawn.cpp
+++ Source/kickstart_code/CollidingPawn.cpp
@@ -3,11 +3,36 @@
 
 #include "CollidingPawn.h"
 
+#include<UObject/ConstructorHelpers.h>
+#include<Particles/ParticleSystem.h>
+#include<Components/SphereComponent.h>
+#include<Camera/CameraComponent.h>
+#include<GameFramework/SpringArmComponent.h>
+
 // Sets default values
 ACollidingPawn::ACollidingPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a sphere object and make it the root component
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent; // A RootComponent is the component that deals with the Transform of an instance of a APawn
+	SphereComponent->InitSphereRadius(40.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	// Associate a Mesh component to the sphere to make it visible
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAsset.Succeeded()) 
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		SphereVisual->SetWorldScale3D(FVector(0.8f));
+	}
+
+
 
 }
 
